2ab720c0335bc63ee176267648b3200fe0630da1
Laura Collins <laura.e.collins1@gmail.com>
2019-09-05 20:58:26 -0500

    added sorting cards

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+const studentArray = [];
 const studentForm = () => {
 
     document.getElementById('jumboInterior').innerHTML = `
@@ -8,8 +9,8 @@ const studentForm = () => {
             <div class="form-inline">
             <div class="form-group">
                 <label for="validationDefault01">Student: </label>
-                <input type="text" class="form-control" id="validationDefault01" placeholder="student name" value="Lilly Potter" required>
-                <button class="btn btn-primary" type="submit" id="sort">Sort!</button>
+                <input type="text" class="form-control" id="formName" placeholder="student name" value="Lilly Potter" required>
+                <button class="btn btn-primary" type="button" id="sort">Sort!</button>
             </div>
             </div>
         
@@ -18,15 +19,46 @@ const studentForm = () => {
 
 }
 
+const printToDom = (toPrint, divId) => {
+    document.getElementById(divId).innerHTML = toPrint;
+};
+
+const studentBuilder = (studArr) => {
+    let domString = '<div class="row">';
+    for (let i = 0; i < studArr.length; i++) {
+        const studentCard = studArr[i];
+        domString += `
+            <div class="card text-center col-sm-4" id="studentCard">
+                <div class="card-body">
+                    <h5 class="card-title" id="name">${studentCard.name}</h5>
+                    <p class="card-text" id="house">${studentCard.house}</p>
+                    <button class="btn btn-primary" id="expel">Expel</button>
+                </div>
+            </div>
+        `;
+    };
+    domString += '</div>';
+    printToDom(domString, 'studentZone');
+}
+
+
 const buttonClick = (e) => {
     const type = e.target.id;
     if (type === 'sortMe') {
         studentForm();
+        document.getElementById('sort').addEventListener('click', buttonClick);
     } else if (type === "sort") {
+        const houses = ['Gryffindor', 'Hufflepuff', 'Ravenclaw', 'Slytherin'];
+        let number = Math.floor(Math.random() * Math.floor(4)); 
+        let result = houses[number];
+        const studentName = document.getElementById('formName').value
+        studentArray.push({name: studentName, house: result});
+        studentBuilder(studentArray);
 
     } else if (type === "expel") {
         
     }
-}
+}       
+
 
 document.getElementById('sortMe').addEventListener('click', buttonClick);
