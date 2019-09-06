405423e7a03b731ede8a8fef2bb34ba472b680b4
Laura Collins <laura.e.collins1@gmail.com>
2019-09-05 20:12:09 -0500

    adding function for buttonClick and studentForm

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
-console.log('sorting hat');
+const studentForm = () => {
+
+    document.getElementById('jumboInterior').innerHTML = `
+        <form id="studentForm">
+            <div class="form-row">
+                <h1>Enter First Year's Name</h1>
+            </div>
+            <div class="form-inline">
+            <div class="form-group">
+                <label for="validationDefault01">Student: </label>
+                <input type="text" class="form-control" id="validationDefault01" placeholder="student name" value="Lilly Potter" required>
+                <button class="btn btn-primary" type="submit" id="sort">Sort!</button>
+            </div>
+            </div>
+        
+        </form>
+    `;
+
+}
+
+const buttonClick = (e) => {
+    const type = e.target.id;
+    if (type === 'sortMe') {
+        studentForm();
+    } else if (type === "sort") {
+
+    } else if (type === "expel") {
+        
+    }
+}
+
+document.getElementById('sortMe').addEventListener('click', buttonClick);
